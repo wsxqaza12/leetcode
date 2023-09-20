dd97307c62971663b77c5b337b5bf94e3a6ca408
chichi <wsxqaza12@gmail.com>
2023-09-20 23:40:41 +0800

    Definde useful ListNode function

--- ListNode/module.py
+++ ListNode/module.py
@@ -2,3 +2,18 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+def check_ListNode_ans(head):
+    while head:
+        print(head.val)
+        head = head.next
+
+def array_to_ListNode(arr):
+    ls = ListNode(-1)
+    dummy = ls
+
+    for i in range(len(arr)):
+        temp_node = ListNode(arr[i])
+        dummy.next = temp_node
+        dummy = dummy.next
+    return ls.next
