b77138a7aac62cbdea741814aef0bd70c2f1cbbf
chichi <wsxqaza12@gmail.com>
2023-12-17 17:30:25 +0800

    daily

--- 242_Valid Anagram.py
+++ 242_Valid Anagram.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isAnagram(self, s, t):
         set_s = set(s)
         set_t = set(t)
-        
+
         if len(s) != len(t):
             return False
 
@@ -10,6 +10,41 @@ class Solution(object):
             for letter in set_s:
                 if s.count(letter) != t.count(letter):
                     return False
+            return True
+        else:
+            return False
+
+# 速度較慢
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_list = [str(x) for x in s]
+        t_list = [str(x) for x in t]
+        s_list.sort()
+        t_list.sort()
+
+        for i in range(len(s)):
+            if s_list[i] != t_list[i]:
+                return False
+        return True
+
+# 字典
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_d = defaultdict(int)
+        t_d = defaultdict(int)
+        for x in s:
+            s_d[x] += 1
+        for x in t:
+            t_d[x] += 1
+
+        if s_d == t_d:
             return True
         else:
             return False
