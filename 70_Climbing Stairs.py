a16078bde291362e161a16e0926342987c99925e
chichi <wsxqaza12@gmail.com>
2024-01-18 23:57:01 +0800

    daily DP2

--- 70_Climbing Stairs.py
+++ 70_Climbing Stairs.py
@@ -19,4 +19,16 @@ class Solution:
 
 
 # DP
-    
+class Solution:
+def climbStairs(self, n: int) -> int:
+
+    step = {}
+    step[0] = 1
+    step[1] = 2
+
+    for i in range(2, n):
+        step[i] = step[i-1] + step[i-2]
+
+    return step[n-1]
+
+            
