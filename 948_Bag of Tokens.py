348fd3721a26d9603a222c51cf863ffae33e8513
chichi <wsxqaza12@gmail.com>
2024-03-05 23:32:09 +0800

    daily

--- 948_Bag of Tokens.py
+++ 948_Bag of Tokens.py
@@ -1,2 +1,18 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        score = 0
+        tokens.sort()
+        if len(tokens) == 0 or power < tokens[0]:
+            return score
+            
+        while len(tokens) != 0:
+            if power >= tokens[0]:
+                power -= tokens.pop(0)
+                score += 1
+            elif score > 0 and len(tokens) != 1:
+                power += tokens.pop()
+                score -= 1
+            else:
+                break
+        
+        return score
