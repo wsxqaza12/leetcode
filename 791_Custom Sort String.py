a2d03bb2ff5e72ca0fa90d92199a135a076b0c49
chichi <wsxqaza12@gmail.com>
2024-03-12 00:04:14 +0800

    daily_revise

--- 791_Custom Sort String.py
+++ 791_Custom Sort String.py
@@ -1,2 +1,19 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        s_dict = {}
+        for char in s:
+            if char not in s_dict:
+                s_dict[char] = 1
+            else:
+                s_dict[char] += 1
+        
+        ans = ''
+        for char in order:
+            if char in s_dict:
+                ans += char * s_dict[char]
+        
+        for char in s:
+            if char not in order:
+                ans += char
+        
+        return ans
